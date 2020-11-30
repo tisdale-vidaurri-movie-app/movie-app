cb0b7cc44895ce49816bf87df1c51516a82a142d
William Tisdale <will.tisdale.31@gmail.com>
2020-11-30 10:20:14 -0600

    finished edit button

--- js/movie-app.js
+++ js/movie-app.js
@@ -8,10 +8,16 @@ $(document).ready( function(){
     const refreshPage = $('.fetch-data')
     const addBtn = $('#addMovie')
     let mainHTML = ""
+    const movieApiURL = `http://www.omdbapi.com/?apikey=${omdbKey}&`
 
     refreshPage.click(() => {
         fetchData()
     })
+
+    // fetch(`http://www.omdbapi.com/?apikey=${omdbKey}?t=SavingPrivateRyan`)
+    //     .then(res => res.json())
+    //     .then(data => console.log(data))
+    //     .catch(error => console.error(error))
 
     const fetchData = () => {
         loading.toggle('hidden')
@@ -32,7 +38,7 @@ $(document).ready( function(){
     const renderHTML = data => {
         mainHTML = ""
         for(let ele of data) {
-            mainHTML += `<div class="col-12 col-sm-6 col-md-4 col-lg-3"
+            mainHTML += `<div id="movie${ele.id}" class="col-12 col-sm-6 col-md-4 col-lg-3"
             <div class="card" style="width: 18rem;">
             <img src="${ele.poster}" class="card-img-top" alt="...">
                 <div class="card-body">
@@ -43,11 +49,82 @@ $(document).ready( function(){
                         <li class="list-group-item">Genres: ${ele.genre}</li>
                     </ul>
                     <p class="card-text">${ele.plot}</p>
+                    <button class="btn btn-primary edit-data${ele.id}">Edit</button>
+                    <div class="edit${ele.id} hidden">
+                    <input id="titleInput${ele.id}" type="text" class="form-control mb-2 mr-sm-2" placeholder="${ele.title}">
+                    <label class="sr-only">Username</label>
+            <div class="input-group mb-2 mr-sm-2">
+                <div class="input-group-prepend">
+                    <div class="input-group-text">Rating</div>
                 </div>
-        </div></div>`
+                <select id="ratingSelect${ele.id}" class="form-control">`
+            console.log(ele.rating);
+            if(ele.rating === '1'){
+                mainHTML += `<option selected>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>`
+            } else if(ele.rating === '2') {
+                 mainHTML += `<option>1</option>
+                    <option selected>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>`
+            } else if(ele.rating === '3') {
+                 mainHTML += `<option>1</option>
+                    <option>2</option>
+                    <option selected>3</option>
+                    <option>4</option>
+                    <option>5</option>`
+            } else if(ele.rating === '4') {
+                 mainHTML += `<option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option selected>4</option>
+                    <option>5</option>`
+            } else if(ele.rating === '5') {
+                 mainHTML += `<option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option selected>5</option>`
+            }
+            mainHTML += `</select>
+            </div>
+            <button id="editMovie${ele.id}" class="btn btn-primary mb-2">Submit</button>
+                </div>
+        </div></div></div>`
         }
         mainRow.html(mainHTML)
+        for(let ele of data) {
+            $(`.edit-data${ele.id}`).click(function () {
+                $(`.edit${ele.id}`).toggle('hidden')
+            })
+            $(`#editMovie${ele.id}`).click(function () {
+                let userTitle = $(`#titleInput${ele.id}`).val()
+                let userRating = $(`#ratingSelect${ele.id}`).val()
+                let userMovie = {
+                    title: userTitle,
+                    rating: userRating
+                }
+                const patchOptions = {
+                    method: 'PATCH',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(userMovie)
+                }
+                fetch(`${url}/${ele.id}`, patchOptions)
+                    .then(res => res.json())
+                    .then(data => console.log(data))
+                    .then(fetchData())
+                    .catch(error => console.error(error))
+            })
+        }
     }
+
+
 
     const deleteOptions = {
                 method: 'DELETE',
@@ -90,5 +167,7 @@ $(document).ready( function(){
             .then(fetchData())
             .catch(error => console.error(error))
     })
+
+
 
 })
